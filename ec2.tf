ef64fdd605c2fd67a5935d810cb6def2ae40edb1
rushidholecom <dholerushi.com@gmail.com>
2026-03-18 18:03:55 +0530

    create ec2

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "terraform-prac" {
+  ami = "ami-09dbc7ce74870d573"
+  instance_type = "t3.micro"
+  key_name = "rushi-rd369-london"
+  vpc_security_group_ids = ["sg-00ac324807d28933a"]
+  tags = {
+    Name = "terraform-prac"
+    env = "dev"
+  }
+}
